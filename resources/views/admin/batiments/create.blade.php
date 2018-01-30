c99f6dfbe3e00d152205b4a1a2328ab6edd7ec73
awagaye <awagaye96@gmail.com>
2018-01-30 03:33:49 +0000

    backoffice

--- resources/views/admin/batiments/create.blade.php
+++ resources/views/admin/batiments/create.blade.php
@@ -16,7 +16,7 @@
 
             <div class="pull-right">
 
-                <a class="btn btn-primary" href="{{ route('batiment.index') }}"> Back</a>
+                <a class="btn btn-primary" href="{{ route('batiment.index') }}"> Retour</a>
 
             </div>
 
@@ -162,7 +162,7 @@
 
         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
 
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Envoyer</button>
 
         </div>
 
